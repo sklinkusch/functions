7771d9338451ee5ede280a210fec07bbffc1d281
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2018-12-17 14:42:32 +0100

    added jquery configuration file

---
+++ js/bootstrap-ex.js
@@ -0,0 +1,8 @@
+// Call Carousel -> carousel
+const carousel = $(".carousel");
+// change behavior
+// carousel.carousel([(interval = 10000)]);
+carousel.carousel({
+  interval: 2500,
+  pause: "hover"
+});
