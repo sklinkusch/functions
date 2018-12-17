874af614c341942c2b911fa56365609de14cb1ee
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2018-12-17 14:58:26 +0100

    removed unnecessary line

--- js/bootstrap-ex.js
+++ js/bootstrap-ex.js
@@ -1,7 +1,6 @@
 // Call Carousel -> carousel
 const carousel = $(".carousel");
 // change behavior
-// carousel.carousel([(interval = 10000)]);
 carousel.carousel({
   interval: 2500,
   pause: "hover"
